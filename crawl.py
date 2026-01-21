9bdf8e63ca203656d71dab813fb0e111a4e6770d
Romas <r2udis@gmail.com>
2026-01-21 02:18:05 +0200

    add extract page data

--- crawl.py
+++ crawl.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 
 def normalize_url(raw_url):
@@ -16,3 +16,34 @@ def get_first_paragraph_from_html(html):
     main = soup.find("main")
     p = main.find("p") if main else soup.find("p")
     return p.get_text().strip() if p else ""
+
+def get_urls_from_html(html, base_url):
+    soup = BeautifulSoup(html, 'html.parser')
+    links = soup.find_all("a")
+    urls = []
+
+    for link in links:
+        href = link.get("href")
+        urls.append(urljoin(base_url, href))
+
+    return urls
+
+def get_images_from_html(html, base_url):
+    soup = BeautifulSoup(html, 'html.parser')
+    images = soup.find_all("img")
+    urls = []
+
+    for image in images:
+        src = image.get("src")
+        urls.append(urljoin(base_url, src))
+
+    return urls
+
+def extract_page_data(html, page_url):
+    return {
+        "url": page_url,
+        "h1": get_h1_from_html(html),
+        "first_paragraph": get_first_paragraph_from_html(html),
+        "outgoing_links": get_urls_from_html(html, page_url),
+        "image_urls": get_images_from_html(html, page_url)
+    }
